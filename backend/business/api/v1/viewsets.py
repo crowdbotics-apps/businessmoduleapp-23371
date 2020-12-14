ae4ffe4dcc11ee2fd78341c9a60b33353c5554af
AndreMachado <csantos.machado@gmail.com>
2020-12-14 17:33:13 -0300

    Update screen

--- backend/business/api/v1/viewsets.py
+++ backend/business/api/v1/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework import authentication
 from business.models import CompanyText
 from .serializers import CompanyTextSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
